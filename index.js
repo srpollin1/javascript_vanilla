85c231bbf9f2367dd2923b9827fad0d0e0a541f8
Eduardo Retamales Rojas <80782448+srpollin1@users.noreply.github.com>
2023-11-10 17:31:45 +0000

    Refactored code to use async/await for fetching data from API

--- index.js
+++ index.js
@@ -346,21 +346,19 @@ console.log("--------------------------------------------------");
 // Async/await INICIO
 
 const ul = document.createElement("ul");
+async function loadData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  console.log(data);
+  data.forEach(function (post) {
+    const li = document.createElement("li");
 
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    data.forEach(function (post) {
-        const li = document.createElement("li");
-        li.innerText = post.title;
-        ul.append(li);
-    });
-    document.body.append(ul);
+    li.innerText = post.title;
+    ul.append(li);
   });
+  document.body.append(ul);
+}
 
-console.log();
+loadData();
 
 // Async/await FIN
