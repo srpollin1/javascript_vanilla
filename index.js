2d2d0a168e5958ae86ab9cdf4485b214c670806b
Eduardo Retamales Rojas <80782448+srpollin1@users.noreply.github.com>
2023-11-10 17:27:37 +0000

    Add dynamic list of post titles using forEach

--- index.js
+++ index.js
@@ -346,7 +346,6 @@ console.log("--------------------------------------------------");
 // Async/await INICIO
 
 const ul = document.createElement("ul");
-const li = document.createElement("li");
 
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then(function (response) {
@@ -354,7 +353,12 @@ fetch("https://jsonplaceholder.typicode.com/posts")
   })
   .then(function (data) {
     console.log(data);
-    data.forEach(function (post) {});
+    data.forEach(function (post) {
+        const li = document.createElement("li");
+        li.innerText = post.title;
+        ul.append(li);
+    });
+    document.body.append(ul);
   });
 
 console.log();
