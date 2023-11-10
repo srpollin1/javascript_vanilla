822e48bc09d3e787ccab73f592016bc40d77f568
Eduardo Retamales Rojas <80782448+srpollin1@users.noreply.github.com>
2023-11-10 13:19:01 +0000

    Add and modify functions in index.js.

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ function hello() {
   console.log("Hello World...");
   console.log("Trabajando con Javascript");
 }
-
 hello();
+
 
 function hello2() {
   return "Hello World con return...";
   // puede retornar cualquier tipo de dato, ejemplo un objeto, un arreglo, un string, un numero, etc.
 }
-
 console.log(hello2());
 
 
@@ -20,7 +19,6 @@ function hello3() {
     return "Hello World con return y funcion anonima...";
   };
 }
-
 console.log(hello3()());
 
 // Funciones de Javascript FIN
